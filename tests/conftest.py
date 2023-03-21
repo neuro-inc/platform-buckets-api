631e171ba1e16905ef38710879fc6bddbd6d17eb
Ivan Zubenko <ivan.zubenko@angloamerican.com>
2023-03-21 12:52:06 +0200

    revert conftest changes

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-pytest_plugins = [
-    "tests.integration.docker",
-    "tests.integration.auth",
-    "tests.integration.moto_server",
-    "tests.integration.minio",
-    "tests.integration.kube",
-]
