20111aa4166fa122b494b6ece93447489909d3f0
parikls <porovozls@gmail.com>
2025-05-21 17:58:32 +0300

    migration job

--- platform_buckets_api/project_based_namespaces_migration.py
+++ platform_buckets_api/project_based_namespaces_migration.py
@@ -1,3 +1,18 @@
+# example usage:
+#
+# python \
+#   migration.py
+#   --dry-run \
+#   --delete-old \
+#   kube \
+#   --endpoint-url=http://cluster-url \
+#   --auth-type=token \
+#   --token-path=/path/to/token \
+#   --ca-path=/path/to/ca
+
+# a --delete-old flag can be omitted if we want first to verify,
+# that resources were migrated into a new namespaces
+
 from __future__ import annotations
 
 import argparse
@@ -5,6 +20,7 @@ import asyncio
 import logging
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Any
 
 from apolo_kube_client.apolo import (
     NO_ORG,
@@ -18,16 +34,14 @@ from neuro_logging import (
     init_logging,
 )
 
+logger = logging.getLogger(__name__)
+
 BUCKETS_ID_LABEL = "platform.neuromation.io/id"
 BUCKETS_OWNER_LABEL = "platform.neuromation.io/owner"
 BUCKETS_CREDENTIALS_NAME_LABEL = "platform.neuromation.io/credentials_name"
 BUCKETS_BUCKET_NAME_LABEL = "platform.neuromation.io/bucket_name"
-
 ORG_NAME_LABEL = "platform.neuromation.io/org_name"
 PROJECT_LABEL = "platform.neuromation.io/project"
-
-
-logger = logging.getLogger(__name__)
 
 DEFAULT_NAMESPACE = "platform-jobs"
 
@@ -61,85 +75,161 @@ async def migrate_buckets(
     dry_run: bool = True,
     delete_old: bool = False,
 ) -> None:
+    # creds are not linked directly to org and proj, so we need this mapping
     bucket_id_to_namespace = await _ensure_credentials_namespaces_are_known(kube_client)
 
     url = user_buckets_url(kube_client)
     response = await kube_client.get(url)
-    for item in response["items"]:
-        metadata = item["metadata"]
-        spec = item["spec"]
-        labels = metadata["labels"]
-
-        project_name = labels.get(PROJECT_LABEL) or labels[BUCKETS_OWNER_LABEL]
-        org_name = labels.get(ORG_NAME_LABEL)
-        if not org_name or org_name.upper() == NO_ORG:
-            org_name = normalize_name(NO_ORG)
-
-        labels[ORG_NAME_LABEL] = org_name
-        labels[PROJECT_LABEL] = project_name
-
-        bucket_id = labels[BUCKETS_ID_LABEL]
-
-        if dry_run:
-            namespace_name = bucket_id_to_namespace[bucket_id]
-            logger.info(f"dry_run: will create namespace {namespace_name}")
-        else:
-            created_namespace = await create_namespace(
-                kube_client, org_name, project_name
-            )
-            namespace_name = created_namespace.name
-
-        payload = {
-            "kind": "UserBucket",
-            "apiVersion": "neuromation.io/v1",
-            "metadata": {
-                "name": metadata["name"],
-                "labels": labels,
-            },
-            "spec": spec,
-        }
-
-        bucket_creation_url = generate_user_buckets_url(kube_client, namespace_name)
-
-        if dry_run:
-            logger.info(f"dry_run: POST {bucket_creation_url}. {payload}")
-        else:
-            await kube_client.post(
-                bucket_creation_url,
-                json=payload,
-            )
+    for old_bucket in response["items"]:
+        await create_bucket(
+            kube_client=kube_client,
+            old_bucket=old_bucket,
+            bucket_id_to_namespace=bucket_id_to_namespace,
+            dry_run=dry_run,
+        )
 
     url = persistent_bucket_credentials_url(kube_client)
     response = await kube_client.get(url)
-    for item in response["items"]:
-        name = item["metadata"]["name"]
-
-        namespace = None
-
-        for bucket_id in item["spec"]["bucket_ids"]:
-            namespace = bucket_id_to_namespace[bucket_id]
-            if namespace:
-                break
-
-        if not namespace:
-            raise RuntimeError()
-
-        payload = {
-            "kind": "PersistentBucketCredential",
-            "apiVersion": "neuromation.io/v1",
-            "metadata": {"name": name, "labels": item["metadata"]["labels"]},
-            "spec": item["spec"],
-        }
-        credentials_creation_url = generate_persistent_bucket_credential_url(
-            kube_client, namespace
+    for old_credentials in response["items"]:
+        await create_credentials(
+            kube_client=kube_client,
+            old_credentials=old_credentials,
+            bucket_id_to_namespace=bucket_id_to_namespace,
+            dry_run=dry_run,
         )
-        if dry_run:
-            logger.info(f"dry_run: POST {credentials_creation_url}. {payload}")
-        else:
-            await kube_client.post(credentials_creation_url, json=payload)
 
     if delete_old:
         await delete_old_buckets(kube_client, dry_run=dry_run)
+
+
+async def create_bucket(
+    kube_client: KubeClient,
+    old_bucket: dict[str, Any],
+    bucket_id_to_namespace: dict[str, str],
+    *,
+    dry_run: bool,
+) -> None:
+    metadata = old_bucket["metadata"]
+    spec = old_bucket["spec"]
+    labels = metadata["labels"]
+
+    project_name = labels.get(PROJECT_LABEL) or labels[BUCKETS_OWNER_LABEL]
+    org_name = labels.get(ORG_NAME_LABEL)
+    if not org_name or normalize_name(org_name) == normalize_name(NO_ORG):
+        org_name = normalize_name(NO_ORG)
+
+    labels[ORG_NAME_LABEL] = org_name
+    labels[PROJECT_LABEL] = project_name
+
+    bucket_id = labels[BUCKETS_ID_LABEL]
+
+    if dry_run:
+        namespace_name = bucket_id_to_namespace[bucket_id]
+        logger.info(f"dry_run: will create namespace {namespace_name}")
+    else:
+        created_namespace = await create_namespace(kube_client, org_name, project_name)
+        namespace_name = created_namespace.name
+
+    payload = {
+        "kind": "UserBucket",
+        "apiVersion": "neuromation.io/v1",
+        "metadata": {
+            "name": metadata["name"],
+            "labels": labels,
+        },
+        "spec": spec,
+    }
+
+    bucket_creation_url = generate_user_buckets_url(kube_client, namespace_name)
+
+    if dry_run:
+        logger.info(f"dry_run: POST {bucket_creation_url}. {payload}")
+    else:
+        await kube_client.post(
+            bucket_creation_url,
+            json=payload,
+        )
+
+
+async def create_credentials(
+    kube_client: KubeClient,
+    old_credentials: dict[str, Any],
+    bucket_id_to_namespace: dict[str, str],
+    *,
+    dry_run: bool,
+):
+    metadata = old_credentials["metadata"]
+    name = metadata["name"]
+
+    namespace = None
+
+    for bucket_id in old_credentials["spec"]["bucket_ids"]:
+        namespace = bucket_id_to_namespace[bucket_id]
+        if namespace:
+            break
+
+    if not namespace:
+        raise RuntimeError()
+
+    payload = {
+        "kind": "PersistentBucketCredential",
+        "apiVersion": "neuromation.io/v1",
+        "metadata": {"name": name, "labels": metadata["labels"]},
+        "spec": old_credentials["spec"],
+    }
+    credentials_creation_url = generate_persistent_bucket_credential_url(
+        kube_client, namespace
+    )
+    if dry_run:
+        logger.info(f"dry_run: POST {credentials_creation_url}. {payload}")
+    else:
+        await kube_client.post(credentials_creation_url, json=payload)
+
+
+async def create_secret(
+    kube_client: KubeClient,
+    old_secret: dict[str, Any],
+    org_name: str,
+    project_name: str,
+    *,
+    dry_run: bool,
+):
+    metadata = old_secret["metadata"]
+    secret_name = metadata["name"]
+
+    labels = metadata.get("labels", {}) or {}
+    labels[ORG_NAME_LABEL] = org_name
+    labels[PROJECT_LABEL] = project_name
+
+    payload = {
+        "kind": "Secret",
+        "apiVersion": "v1",
+        "type": old_secret["type"],
+        "data": old_secret["data"],
+        "metadata": {
+            "name": secret_name,
+            "labels": labels,
+        },
+    }
+    if "spec" in old_secret:
+        payload["spec"] = old_secret["spec"]
+    if dry_run:
+        org_name = normalize_name(org_name)
+        project_name = normalize_name(project_name)
+        namespace_name = generate_namespace_name(org_name, project_name)
+        logger.info(f"dry_run: will create namespace {namespace_name}")
+    else:
+        created_namespace = await create_namespace(kube_client, org_name, project_name)
+        namespace_name = created_namespace.name
+
+    secret_creation_url = generate_secrets_url(kube_client, namespace_name)
+    if dry_run:
+        logger.info(f"dry_run: POST {secret_creation_url}. {payload}")
+    else:
+        await kube_client.post(
+            secret_creation_url,
+            json=payload,
+        )
 
 
 async def migrate_secrets(
@@ -147,7 +237,54 @@ async def migrate_secrets(
     dry_run: bool = True,
     delete_old: bool = False,
 ) -> None:
-    pass
+    url = generate_secrets_url(kube_client, namespace=DEFAULT_NAMESPACE)
+    response = await kube_client.get(url)
+
+    scope = []
+
+    # let's gather a scope of work
+    for item in response["items"]:
+        secret_name = item["metadata"]["name"]
+        if not secret_name.startswith("project"):
+            continue
+
+        parts = secret_name.split("--")
+
+        # we need to ensure that all secret names are parseable,
+        # and we can extract an org and project names
+        if len(parts) == 3:
+            org_name = normalize_name(NO_ORG)
+            project_name = parts[1]
+        elif len(parts) == 4:
+            org_name, project_name = parts[1], parts[2]
+        else:
+            org_name = project_name = None
+
+        scope.append((item, org_name, project_name))
+
+    # a sanity-check to ensure that all secrets are migratable
+    failed_secrets = []
+    for item, org_name, project_name in scope:
+        if not org_name or not project_name:
+            failed_secrets.append(item)
+    if failed_secrets:
+        raise RuntimeError(
+            f"unable to extract proper org/project names from secrets: "
+            f"{', '.join([x['metadata']['name'] for x in failed_secrets])}"
+        )
+
+    # create secrets in a new namespaces
+    for old_secret, org_name, project_name in scope:
+        await create_secret(
+            kube_client=kube_client,
+            old_secret=old_secret,
+            org_name=org_name,
+            project_name=project_name,
+            dry_run=dry_run,
+        )
+
+    if delete_old:
+        await delete_old_secrets(kube_client, dry_run)
 
 
 def apis_url(kube_client: KubeClient) -> str:
@@ -173,6 +310,17 @@ def generate_user_buckets_url(
 
 def persistent_bucket_credentials_url(kube_client: KubeClient) -> str:
     return f"{neuromation_url(kube_client)}/persistentbucketcredentials"
+
+
+def generate_secrets_url(
+    kube_client: KubeClient,
+    namespace: str,
+    name: str | None = None,
+) -> str:
+    url = f"{kube_client._base_url}/api/v1/namespaces/{namespace}/secrets"
+    if name:
+        url = f"{url}/{name}"
+    return url
 
 
 def generate_persistent_bucket_credential_url(
@@ -244,7 +392,7 @@ async def delete_old_buckets(
     dry_run: bool = True,
 ) -> None:
     """
-    Delete old resources in a default namespace
+    Delete buckets/credentials in a default namespace
     """
     url = generate_user_buckets_url(kube_client, namespace=DEFAULT_NAMESPACE)
     response = await kube_client.get(url)
@@ -268,6 +416,25 @@ async def delete_old_buckets(
             kube_client,
             namespace=DEFAULT_NAMESPACE,
             name=item["metadata"]["name"],
+        )
+        if dry_run:
+            logger.info(f"dry_run: DELETE {deletion_url}")
+        else:
+            await kube_client.delete(deletion_url)
+
+
+async def delete_old_secrets(
+    kube_client: KubeClient,
+    dry_run: bool = True,
+) -> None:
+    """
+    Delete secrets in a default namespace
+    """
+    url = generate_secrets_url(kube_client, namespace=DEFAULT_NAMESPACE)
+    response = await kube_client.get(url)
+    for item in response["items"]:
+        deletion_url = generate_secrets_url(
+            kube_client, DEFAULT_NAMESPACE, item["metadata"]["name"]
         )
         if dry_run:
             logger.info(f"dry_run: DELETE {deletion_url}")
